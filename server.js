d932146a4017e36d87fc5dc90626b1d9334c7a93
Dawei Dai <shachopin@gmail.com>
2022-06-17 15:45:25 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+import 'express-async-errors';
+import morgan from 'morgan';
+
+app.use(express.json())
+app.get('*', (req, res) => {
+  res.json({hello: 'wolrd'});
+});
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`)
+})
