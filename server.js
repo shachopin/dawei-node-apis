7b41c2a8984cbca042fdcbcb5546cf36bd4cb6d9
Glitch (dawei-node-apis) <none>
2022-06-18 02:15:47 +0000

    6️⃣🚍 Updated with Glitch

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 import authRouter from './routes/authRoutes.js'
-
+import notFoundMiddleware from './middleware/not-found.js'
 app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
 // app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
